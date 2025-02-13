7a65fbf8f094293dc84c7a9257788837ea1de4e7
frostyx-vk <vk.xam@yandex.ru>
2025-02-13 21:41:11 +0700

    add progress bar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,9 +1,16 @@
 import s from './ProgressBar.module.css'
 
 function ProgressBar() {
+  let count = 10;
   return (
-    <div>
-
+    <div className={`${s.barContainer} ${count === null ? s.hide : ''}`}>
+      <progress
+        className={s.barProgress}
+        max="10"
+      />
+      <span className={s.barText}>
+        Отправка данных через {count} секунд
+      </span>
     </div>
   )
 }
