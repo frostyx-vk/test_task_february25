d96e836e974998dae749268ac749cbfe3102a320
frostyx-vk <vk.xam@yandex.ru>
2025-02-13 21:23:17 +0700

    logic for table with validations

--- src/components/BodyTable/BodyTable.jsx
+++ src/components/BodyTable/BodyTable.jsx
@@ -1,6 +1,39 @@
 import s from './BodyTable.module.css'
+import React, { useCallback } from 'react';
 
-function BodyTable({ tbodyData }) {
+function BodyTable({ tbodyData, editCell }) {
+
+    const handleInputChange = useCallback((id, type) => (e) => {
+        const masks = {
+            number: /^(?:\d+(\.\d*)?)?$/
+        };
+
+        const validateInputs = (value, type) => {
+            if (type === 'number') {
+                const validateValue = value.replace('.', '');
+                if (masks.number.test(validateValue)) {
+                    return validateValue
+                };
+                return null;
+            };
+
+            if (type === 'percent') {
+                const percentValue = value.slice(0, -1).replace(',', '.');
+                if (masks.number.test(percentValue)) {
+                    return percentValue;
+                };
+                return null;
+            };
+
+            if (type === 'text') return value;
+
+            return null;
+        };
+
+        const сheckingValue = validateInputs(e.target.value, type);
+
+        сheckingValue !== null && editCell(id, сheckingValue)
+    }, [editCell]);
 
     return (
         <tbody>
@@ -17,12 +50,13 @@ function BodyTable({ tbodyData }) {
                                 <textarea
                                     className={s.bodyTextarea}
                                     value={value}
+                                    onChange={handleInputChange(id, 'text')}
                                 />
                             ) : (
                                 <input
                                     className={`${s.bodyInput} ${id === 13 ? s.changeColor : ''}`}
-                                    value={value}
-                                    type={type}
+                                    value={type === 'percent' ? `${value}%` : value}
+                                    onChange={handleInputChange(id, type)}
                                 />
                             )}
                         </td>
--- src/components/EditingTable/EditingTable.jsx
+++ src/components/EditingTable/EditingTable.jsx
@@ -4,17 +4,35 @@ import BodyTable from '../BodyTable/BodyTable'
 import ProgressBar from './../ProgressBar/ProgressBar'
 import { initialTableData } from '../../store/initialTableData'
 
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 function EditingTable() {
 
   const [dataTable, setTableData] = useState(initialTableData);
 
+  const editCell = useCallback((id, value, isThead = false) => {
+    let newData;
+
+    if (isThead) {
+      const newThead = dataTable.theadData.map((data) =>
+        data.id === id ? { ...data, value } : data
+      );
+      newData = { ...dataTable, theadData: newThead };
+    } else {
+      const newTbody = dataTable.tbodyData.map((rows) => ({
+        row: rows.row.map((row) => (row.id === id ? { ...row, value } : row)),
+      }));
+      newData = { ...dataTable, tbodyData: newTbody };
+    }
+
+    setTableData(newData);
+  }, [dataTable]);
+
   return (
     <div className={s.tableContainer}>
       <table className={s.editingTable}>
-        <HeaderTable theadData={dataTable.theadData} />
-        <BodyTable tbodyData={dataTable.tbodyData} />
+        <HeaderTable theadData={dataTable.theadData} editCell={editCell} />
+        <BodyTable tbodyData={dataTable.tbodyData} editCell={editCell} />
       </table>
       <ProgressBar />
     </div>
